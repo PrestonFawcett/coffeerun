c0a55c154c8288fa8aff6267f56b75474529492d
PrestonFawcett <83142528+PrestonFawcett@users.noreply.github.com>
2021-09-30 18:26:27 -0700

    Update paymenthandler.js

--- scripts/paymenthandler.js
+++ scripts/paymenthandler.js
@@ -6,13 +6,10 @@
   var formHandler = new FormHandler(FORM_SELECTOR);
 
   formHandler.addSubmitHandler(function (data) {
-    //eslint-disable-next-line no-undef
     $("#titleHolder").text(data.title);
-    //eslint-disable-next-line no-undef
     $("#nameHolder").text(data.name);
-    //eslint-disable-next-line no-undef
     $("#thankUMSG").modal();
   });
-  //eslint-disable-next-line no-console
+  
   console.log(formHandler);
 })(window);
